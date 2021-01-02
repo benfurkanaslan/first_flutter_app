ca9a1cd94eae4bb5c19457f74c824e468822054b
benfurkanaslan <60846263+benfurkanaslan@users.noreply.github.com>
2021-01-02 15:16:05 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/fuasl/flutter_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.fuasl.flutter_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var screenInfo = MediaQuery.of(context);
+    final screenWidth = screenInfo.size.width;
+    print(screenWidth);
+    final screenHeight = screenInfo.size.height;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Column(
+            children: [
+              Container(
+                child: MaterialButton(
+                  onPressed: () {
+                    print("resme basıldı");
+                  },
+                  child: Image.network(
+                    "https://mediatrend.mediamarkt.com.tr/wp-content/uploads/2017/02/2017_subat_03.jpg",
+                    width: screenWidth,
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text("button"),
+                  ),
+                  width: 100,
+                  height: 100,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  child: MaterialButton(
+                    onPressed: () {},
+                    child: Text("Material Button"),
+                  ),
+                  width: 100,
+                  height: 100,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  child: FlatButton(
+                    onPressed: () {},
+                    child: Text("press"),
+                  ),
+                  width: 100,
+                  height: 100,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  child: OutlineButton(
+                    highlightedBorderColor: Colors.greenAccent,
+                    onPressed: () {},
+                    child: Text("Outline Button"),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: Text("Elevated Button"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
